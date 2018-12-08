ca478aed68f0ebd527c995058f6f480fc725816c
ZhannaL <zhannet1814@gmail.com>
2018-12-08 19:47:59 +0100

    fix css uielement checkbox

--- src/hud/uiElements/Checkbox/Checkbox.stories.jsx
+++ src/hud/uiElements/Checkbox/Checkbox.stories.jsx
@@ -1,7 +1,6 @@
 // @flow
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
 import Checkbox from './Checkbox';
 
 storiesOf('Checkbox', module)
