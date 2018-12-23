76e7cd7996f1cb8e9a842828d0c87eb885fd8f96
ZhannaL <zhannet1814@gmail.com>
2018-12-23 17:22:26 +0100

    create uielement input range (#16)

---
+++ src/hud/uiElements/InputRange/InputRange.stories.jsx
@@ -0,0 +1,11 @@
+// @flow strct
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import InputRange from './InputRange';
+
+storiesOf('Input Range', module)
+  .add('Input Range', () =>
+  <>
+    <br />
+    <InputRange />
+  </>);
---
+++ src/hud/uiElements/InputRange/index.js
@@ -0,0 +1,2 @@
+// @flow strct
+export { default } from './InputRange';
