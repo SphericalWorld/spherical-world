77b2b57d3204973a12deeff494e49a6df15a40a5
ZhannaL <zhannet1814@gmail.com>
2019-01-06 12:51:11 +0100

    fix css border-radius, added modalWindowMenu to storybook (#30)

---
+++ src/hud/components/ModalWindowMenu/ModalWindowMenu.stories.jsx
@@ -0,0 +1,23 @@
+// @flow strct
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import ModalWindowMenu from './ModalWindowMenu';
+
+storiesOf('Modal Window Menu', module)
+  .add('Modal Window Menu', () => (
+    <ModalWindowMenu caption="Header text">
+      {
+        new Array(5).fill(
+          'content Menu',
+        )
+      }
+      {
+        new Array(5).fill(
+          <>
+            <br />
+            content Menu
+          </>,
+        )
+      }
+    </ModalWindowMenu>
+  ));
