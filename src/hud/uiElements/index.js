523815d16fc80f2741dc10af6b5ec661e0bdf358
ZhannaL <zhannet1814@gmail.com>
2019-01-17 19:12:16 +0100

    create uielement ProgressBar

--- src/hud/uiElements/index.js
+++ src/hud/uiElements/index.js
@@ -6,3 +6,4 @@ export { default as InventorySlot } from './InventorySlot';
 export { default as Label } from './Label';
 export { default as ModalWindow } from './ModalWindow';
 export { default as Select } from './Select';
+export { default as ProgressBar } from './ProgressBar';
